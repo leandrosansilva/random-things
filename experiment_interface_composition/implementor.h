bc2cbdbb23ef93d0224d2fff754e3599861ebede
Leandro Santiago <leandrosansilva@gmail.com>
2018-03-24 12:39:51 +0100

    clean up on header

--- experiment_interface_composition/implementor.h
+++ experiment_interface_composition/implementor.h
@@ -1,16 +1,9 @@
-//#include <brigand/sequences/set.hpp>
-
 namespace detail {
   template<typename... T>
-  struct compose_wrapper: virtual T...
-  {
-  };
-
-  template<typename... T>
-  struct compose_parents;
+  struct compose_interfaces;
 
   template<typename A>
-  struct compose_parents<A>
+  struct compose_interfaces<A>
   {
     struct type: virtual A
     {
@@ -18,121 +11,57 @@ namespace detail {
   };
 
   template<typename A, typename B>
-  struct compose_parents<A, B>
+  struct compose_interfaces<A, B>
   {
-    struct type: virtual compose_parents<A>::type,
-                 virtual compose_parents<B>::type
+    struct type: virtual compose_interfaces<A>::type,
+                 virtual compose_interfaces<B>::type
     {
     };
   };
 
   template<typename A, typename B, typename C>
-  struct compose_parents<A, B, C>
+  struct compose_interfaces<A, B, C>
   {
-    struct type: virtual compose_parents<A, B>::type,
-                 virtual compose_parents<A, C>::type,
-                 virtual compose_parents<B, C>::type
+    struct type: virtual compose_interfaces<A, B>::type,
+                 virtual compose_interfaces<A, C>::type,
+                 virtual compose_interfaces<B, C>::type
     {
     };
   };
 
   template<typename A, typename B, typename C, typename D>
-  struct compose_parents<A, B, C, D>
+  struct compose_interfaces<A, B, C, D>
   {
-    struct type: virtual compose_parents<A, B, C>::type,
-                 virtual compose_parents<A, B, D>::type,
-                 virtual compose_parents<A, C, D>::type,
-                 virtual compose_parents<B, C, D>::type
+    struct type: virtual compose_interfaces<A, B, C>::type,
+                 virtual compose_interfaces<A, B, D>::type,
+                 virtual compose_interfaces<A, C, D>::type,
+                 virtual compose_interfaces<B, C, D>::type
     {
     };
   };
 
   template<typename A, typename B, typename C, typename D, typename E>
-  struct compose_parents<A, B, C, D, E>
+  struct compose_interfaces<A, B, C, D, E>
   {
-    struct type: virtual compose_parents<A, B, C, D>::type,
-                 virtual compose_parents<A, B, C, E>::type,
-                 virtual compose_parents<A, C, D, E>::type,
-                 virtual compose_parents<B, C, D, E>::type,
-                 virtual compose_parents<A, B, D, E>::type
+    struct type: virtual compose_interfaces<A, B, C, D>::type,
+                 virtual compose_interfaces<A, B, C, E>::type,
+                 virtual compose_interfaces<A, C, D, E>::type,
+                 virtual compose_interfaces<B, C, D, E>::type,
+                 virtual compose_interfaces<A, B, D, E>::type
     {
     };
   };
 }
 
+/**
+ * FIXME: there's a obvious limitation in this implementation that is the fact that
+ * implements<A, B> should be equal to implements<B, A>, but I confess I don't know how
+ * to implement it. The obvious solution would be to sort tye type list, so it'd behave as
+ * a ordered set, but I am pretty sure this is not possible in C++ as there's no order 
+ * relation beween sets. Unless...
+ * one implements some order metafunction like
+ * before_than<A, B>, before_than<B, C>, which implies before_than<A, C>, and so on...
+ * but I cannot someone doing it in system with thousands of interfaces...
+*/
 template<typename... T>
-using compose = typename detail::compose_parents<T...>::type;
-
-struct I1
-{
-  virtual void doI1() = 0;
-  virtual ~I1() noexcept = default;
-};
-
-struct I2
-{
-  virtual void doI2() = 0;
-  virtual ~I2() noexcept = default;
-};
-
-struct I3
-{
-  virtual void doI3() = 0;
-  virtual ~I3() noexcept = default;
-};
-
-struct I4
-{
-  virtual void doI4() = 0;
-  virtual ~I4() noexcept = default;
-};
-
-struct I5
-{
-  virtual void doI5() = 0;
-  virtual ~I5() noexcept = default;
-};
-
-inline void f(compose<I1>& a)
-{
-  a.doI1();
-}
-
-inline void f2(compose<I3, I4>& a)
-{
-  a.doI4();
-}
-
-struct C1: compose<I1, I2, I3, I4>
-{
-  void doI1() final {}
-  void doI2() final {}
-  void doI3() final {}
-  void doI4() final {}
-};
-
-struct C2: compose<I1, I2, I3, I4, I5>
-{
-  void doI1() final {}
-  void doI2() final {}
-  void doI3() final {}
-  void doI4() final {}
-  void doI5() final {}
-};
-
-struct C3: compose<I1, I2>
-{
-  void doI1() final {}
-  void doI2() final {}
-};
-
-int main(int, char**)
-{
-  auto c1 = C1{};
-  auto c2 = C2{};
-  auto c3 = C3{};
-
-  f2(c2);
-
-  return 0;
-}
+using implements = typename detail::compose_interfaces<T...>::type;
